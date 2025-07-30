0a3302f0e76f25e9fbc9861a71cf9a6b7c43db86
Oluwafikayo Ajayi <o.oluwafikayoajayi@gmail.com>
2025-07-30 10:31:31 +0100

    added the user models and functions

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -20,3 +21,10 @@ server.listen(8080, () => {
     console.log('Server running on http://localhost:8080/');
 });
 
+const MONGO_URL = 'mongodb+srv://ajafik:ajafik00@cluster0.renzqif.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => {
+    console.log(error);
+})
